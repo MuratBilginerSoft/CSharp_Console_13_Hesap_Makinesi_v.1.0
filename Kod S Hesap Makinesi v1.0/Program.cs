3ea2c0b6992a46b69caddca67f2b992a6602566f
Murat Bilginer <muratbilginer09@gmail.com>
2020-03-23 02:25:59 +0300

    Initial commit

---
+++ Kod S Hesap Makinesi v1.0/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Kod_S_Hesap_Makinesi_v1._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Birinci Sayıyı Giriniz: ");
+            int Sayi1 = int.Parse(Console.ReadLine());
+
+            Console.Write("İkinci Sayıyı Giriniz: ");
+            int Sayi2 = int.Parse(Console.ReadLine());
+
+            Console.Write("İŞLEM SEÇ (+ - / *) : ");
+            char Islem = char.Parse(Console.ReadLine());
+
+            if (Islem == '+')
+            {
+                Console.WriteLine("SONUÇ = " + (Sayi1 + Sayi2));
+            }
+            else if (Islem == '-')
+            {
+                Console.WriteLine("SONUÇ = " + (Sayi1 - Sayi2));
+            }
+            else if (Islem == '*')
+            {
+                Console.WriteLine("SONUÇ = " + (Sayi1 * Sayi2));
+            }
+            else if (Islem == '/')
+            {
+                Console.WriteLine("SONUÇ = " + (Sayi1 / Sayi2));
+            }
+            else
+            {
+                Console.WriteLine("Hatalı İşlem");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# CSharp_Console_13_Hesap_Makinesi_v.1.0
+ 
